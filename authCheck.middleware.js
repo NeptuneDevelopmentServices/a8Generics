605bb9f31d91b5fbe53170bffb14f9c4478287f0
Simon Maxwell <67204157+SimonDMaxwell@users.noreply.github.com>
2022-09-06 01:28:09 -0500

    Update authCheck.middleware.js

--- authCheck.middleware.js
+++ authCheck.middleware.js
@@ -1,30 +1,30 @@
-const JSONResponse = require('./json.helper')
+const JSONResponse = require('../../../lib/json.helper')
+const JWTHelper = require('../../../lib/jwt.helper')
 
 const authCheck = (types) => {
 	for (let index = 0; index < types.length; index++) {
 		switch (types[index]) {
-			case 'driver':
+			case 'type1':
 				types[index] = 0
 				break
-			case 'user':
+			case 'type2':
 				types[index] = 1
-				break
-			case 'admin':
-				types[index] = 2
 				break
 		}
 	}
-	return (req, res, next) => {
-		let type = req.session.type
-		if (types.includes(type)) {
-			next()
-		} else {
-			JSONResponse.error(
-				res,
-				401,
-				'Access denied! Please log in, or access this route through the appropriate means.'
-			)
-		}
+	return async (req, res, next) => {
+		JWTHelper.getToken(req, res, 'jwt_auth', (deco) => {
+			if (types.includes(deco.type)) {
+				next()
+			} else {
+				JSONResponse.error(
+					req,
+					res,
+					401,
+					'Access denied! Please log in, or access this route through the appropriate means.'
+				)
+			}
+		})
 	}
 }
 
