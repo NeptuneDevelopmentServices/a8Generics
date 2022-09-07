69852c1ef0e9c6cdaf5ff5caadf89bb9cae69b64
Simon Maxwell <67204157+SimonDMaxwell@users.noreply.github.com>
2022-09-06 21:20:42 -0500

    Create Cookie.service.ts

---
+++ Cookie.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core'
+
+@Injectable({
+	providedIn: 'root',
+})
+export class CookieService {
+	constructor() {}
+
+	setCookie(cname: string, cvalue: string, exdays = 1000 * 60 * 60 * 24) {
+		const d = new Date()
+		d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000)
+		let expires = 'expires=' + d.toUTCString()
+		document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/'
+	}
+
+	getCookie(cname: string) {
+		let name = cname + '='
+		let decodedCookie = decodeURIComponent(document.cookie)
+		let ca = decodedCookie.split(';')
+		for (let i = 0; i < ca.length; i++) {
+			let c = ca[i]
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1)
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length)
+			}
+		}
+		return ''
+	}
+}
